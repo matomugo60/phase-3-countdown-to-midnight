b564b1b833751880978d16b2e849c5cf869c1f33
matomugo60 <matomugo60@gmail.com>
2023-02-07 15:46:24 +0300

    Complete

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
 #write your code here
 
-def countdown
+def countdown(num)
+   num.downto(1) do |i|
+    puts "#{i} SECOND(S)!"
+    
+   end
+    return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(num)
+    num.downto(1) do |i|
+        puts "#{i} SECOND(S)!"
+        sleep(1)
+    end
+    "HAPPY NEW YEAR!"
 end
